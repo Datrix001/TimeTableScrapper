c7737d99c18190088b5c38ca5a1f73cf380cb808
Datrix001 <mayankmoullisingh2004@gmail.com>
2026-01-18 12:08:40 +0530

    Added Screenshots

--- lib/features/data/constansts/api_constants.dart
+++ lib/features/data/constansts/api_constants.dart
@@ -4,5 +4,5 @@ final class ApiConstants {
   static const timetableBaseUrl = "http://time-table.sicsr.ac.in/day.php";
 
   static const scriptUrl =
-      "https://script.google.com/macros/s/{YourApiID}/exec";
+      "https://script.google.com/macros/s/AKfycbxH6oYSF5nlJ1dsEeNZEIsDhuqNV2jFOAbe7PUfrfD9Go5LUCGC56BC2ToQI4TsVff8/exec";
 }
